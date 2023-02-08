38ed83041135f0b4ece0568fd49c5d305a4bd31a
Jabeimon <118595164+Jabeimon@users.noreply.github.com>
2023-02-08 14:16:17 +0100

    Add files via upload

--- Proyecto C/Jugador1.gd
+++ Proyecto C/Jugador1.gd
@@ -8,22 +8,29 @@ var puede_saltar = false
 var esta_en_agua = false
 enum estados {idle, cuerpo_tierra, sumergido}
 var estado_actual = estados.idle
+export(PackedScene) var bala_comun
+export(Vector2) var spawnB_izq
+export(Vector2) var spawnB_arr
+export(Vector2) var spawnB_izqAR
+export(Vector2) var spawnB_izqAB
 
 func _ready():
-	pass
+	spawnB_izq = get_node("cuerpo_j1/SpawnBala").position
 
 func _physics_process(delta):
 	
 	Velocidad.y += GRAVEDAD * delta
 	
-	
 	if(Input.is_action_pressed("tecla_a") && estado_actual != estados.cuerpo_tierra && estado_actual != estados.sumergido):
 		Velocidad.x = -VEL_MOVIMIENTO
+		get_node("cuerpo_j1/SpawnBala").position = spawnB_izq
 		get_node("cuerpo_j1/Sprite_J1").flip_h = false
 		if(Input.is_action_pressed("tecla_w") && (puede_saltar) && !esta_en_agua):
 			get_node("animacion_J1").play("J1_diagArriba")
+			get_node("cuerpo_j1/SpawnBala").position = spawnB_izqAR
 		elif(Input.is_action_pressed("tecla_s") && (puede_saltar) && !esta_en_agua):	
 			get_node("animacion_J1").play("J1_diagAbajo")
+			get_node("cuerpo_j1/SpawnBala").position = spawnB_izqAB
 		elif(Input.is_action_just_released("tecla_w") && (puede_saltar) && !esta_en_agua || Input.is_action_just_released("tecla_s") && (puede_saltar) && !esta_en_agua):
 			get_node("animacion_J1").stop()
 		elif(!get_node("animacion_J1").is_playing() && (puede_saltar)):
@@ -34,11 +41,14 @@ func _physics_process(delta):
 			
 	elif(Input.is_action_pressed("tecla_d") && estado_actual != estados.cuerpo_tierra && estado_actual != estados.sumergido):
 		Velocidad.x = VEL_MOVIMIENTO
+		get_node("cuerpo_j1/SpawnBala").position = Vector2(spawnB_izq.x * -1, spawnB_izq.y)
 		get_node("cuerpo_j1/Sprite_J1").flip_h = true
 		if(Input.is_action_pressed("tecla_w") && (puede_saltar) && !esta_en_agua):
 			get_node("animacion_J1").play("J1_diagArriba")
+			get_node("cuerpo_j1/SpawnBala").position = Vector2(spawnB_izqAR.x * -1, spawnB_izqAR.y)
 		elif(Input.is_action_pressed("tecla_s") && (puede_saltar) && !esta_en_agua):	
 			get_node("animacion_J1").play("J1_diagAbajo")
+			get_node("cuerpo_j1/SpawnBala").position = Vector2(spawnB_izqAB.x * -1, spawnB_izqAB.y)
 		elif(Input.is_action_just_released("tecla_w") && (puede_saltar) && !esta_en_agua|| Input.is_action_just_released("tecla_s") && (puede_saltar) && !esta_en_agua):
 			get_node("animacion_J1").stop()
 		elif(!get_node("animacion_J1").is_playing() && (puede_saltar)):
@@ -50,6 +60,7 @@ func _physics_process(delta):
 	elif(Input.is_action_pressed("tecla_w") && estado_actual != estados.cuerpo_tierra && estado_actual != estados.sumergido):
 		if(puede_saltar && !esta_en_agua):
 			get_node("animacion_J1").play("J1_haciaArriba")
+			get_node("cuerpo_j1/SpawnBala").position = spawnB_arr
 		Velocidad.x = 0
 	elif(Input.is_action_pressed("tecla_s")):
 		if(puede_saltar):
@@ -58,7 +69,8 @@ func _physics_process(delta):
 				estado_actual = estados.cuerpo_tierra
 			else:
 				get_node("animacion_J1").play("J1_bajoagua")
-				
+		else:
+			get_node("cuerpo_j1/SpawnBala").position = Vector2(spawnB_arr.x, spawnB_arr.y * -1)
 		Velocidad.x = 0
 	else:
 		Velocidad.x = 0
@@ -81,6 +93,11 @@ func _physics_process(delta):
 			get_node("animacion_J1").play("J1_idle")
 			
 		puede_saltar = false
+	
+	if(Input.is_action_pressed("tecla_x")):
+		var newBala = bala_comun.instance()
+		newBala.global_position = get_node("cuerpo_j1/SpawnBala").global_position
+		get_tree().get_nodes_in_group("main")[0].add_child(newBala)
 	
 	var movimiento = Velocidad * delta
 	get_node("cuerpo_j1").move_and_slide(movimiento)
